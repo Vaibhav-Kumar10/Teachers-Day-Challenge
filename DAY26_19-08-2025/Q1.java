3d25d3b148aadaeb4973d9e98e652052e5cdc0a6
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-08-19 23:01:28 +0530

    Day 26

---
+++ DAY26_19-08-2025/Q1.java
@@ -0,0 +1,18 @@
+class Solution
+{
+public
+    int maxSubArray(int[] nums)
+    {
+        int maxSum = Integer.MIN_VALUE;
+        int n = nums.length;
+        int curSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            curSum += nums[i];
+            maxSum = Math.max(maxSum, curSum);
+            if (curSum < 0)
+                curSum = 0;
+        }
+        return maxSum;
+    }
+}
